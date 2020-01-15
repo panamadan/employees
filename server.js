27a925d4f920e3e174abb4095d02250a2d3f8de6
Juan De La Guardia <delaguardia@outlook.com>
2020-01-14 21:50:30 -0500

    switch function update

--- server.js
+++ server.js
@@ -12,26 +12,53 @@ connection.connect((err) => {
 
 });
 
-function mainApp(){
-    prompt([{
-        type : "list",
-        name : "firstQuestion"
-        message : "what would you like to do?"
-        choices :
-    }])
-    .then(function(answer){
-        switch(answer.firstQuestion){
-            case "view all employees":
-                viewAllEmployess()
-                break;
-                case " jndfdng"
-        
+function mainApp() {
+    inquirer
+        .prompt([{
+            type: "list",
+            name: "firstQuestion",
+            message: "what would you like to do?",
+            choices: [
+                "View all employess",
+                "View employees by department",
+                "Add new employee",
+                "update employee role",
+                "delete employee"
 
-        }
+            ]
+        }])
+        .then(function (answer) {
+            switch (answer.firstQuestion) {
+                case "view all employees":
+                    viewAllEmployess()
+                    break;
+                case "View employees by department":
+                    viewEmployeesDept()
+                    break;
+                case "Add new employee":
+                    addEmployee()
+                    break;
+                case "update employee role":
+                    upddateEmployeeRole()
+                    break;
+                case "delete employee":
+                    deleteEmployee()
+                    break;
 
-    })
-function viewAllEmployess() {
-    
-}
-    
+            }
+
+        })
+    function viewAllEmployess() {
+
+    };
+    function viewEmployeesDept() {
+
+    };
+    function addEmployee() {
+
+    };
+    function deleteEmployee() {
+
+    }
+
 }
